b5541c5bf8e93d569471ae746ff4e3a1d39323b6
SHAH SAHIL HIRAL <sahilshah@cse.iitb.ac.in>
2018-07-23 17:46:17 +0530

    thrown out of sl2

---
+++ lab2/lab2/p7.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+int main(int argc, char* argv[])
+{
+	int pid = fork();
+	int numIter = (int)
+
+
+
+
+	return 0;
+}
